39f6097c1d1ce5b2e04f04c44728cb54e9c3ade3
LingxiaChen <Lingxia.Chen@microsoft.com>
2020-05-25 17:15:30 +0800

    first commit.

---
+++ CA5391CoreTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CA5391CoreTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
